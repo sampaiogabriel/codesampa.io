6e8d92b4150b7c580ab383ded2fd8974c0c8b7a0
sampaiogabriel <gabrielsampaiolima@hotmail.com>
2025-11-28 10:30:27 -0300

    fix: ajust config in next.config.ts to work next-intl

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -1,9 +1,11 @@
-export default function Home() {
-  
+import { getTranslations } from 'next-intl/server';
+
+export default async function Home() {
+  const t = await getTranslations();
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-
+      {t('hello-world')}
     </div>
   );
 }
