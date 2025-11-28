a5c31866d773817b673bb9f73110a60466a82783
sampaiogabriel <gabrielsampaiolima@hotmail.com>
2025-11-28 14:08:05 -0300

    fix: ajust padding and mx-auto

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -3,5 +3,5 @@ import { getTranslations } from 'next-intl/server';
 export default async function Home() {
   const t = await getTranslations('Pages.Home');
   
-  return <div className="container pt-4">{t('hello_world')}</div>;
+  return <div className="container mx-auto pt-4">{t('hello_world')}</div>;
 }
