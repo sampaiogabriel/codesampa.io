dc4fd1f0911c2bde3f92134f2111712ec15eac79
sampaiogabriel <gabrielsampaiolima@hotmail.com>
2025-11-29 13:39:34 -0300

    fix: remove extra symbols

--- src/components/pages/home/shape-shifter/components.tsx
+++ src/components/pages/home/shape-shifter/components.tsx
@@ -267,28 +267,6 @@ export const DarkChart = () => (
   </div>
 );
 
-// --- Floating Symbol (Independent Animation) ---
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const FloatingSymbol = ({ item }: { item: any }) => {
-  return (
-    <motion.div
-      style={{ top: item.top, left: item.left }}
-      className={`absolute z-0 select-none font-mono font-bold ${item.size} ${item.color} pointer-events-none opacity-20`}
-      animate={{
-        y: [0, -20, 0],
-        rotate: [0, 5, -5, 0],
-      }}
-      transition={{
-        duration: 4 + item.depth, 
-        repeat: Infinity,
-        ease: 'easeInOut'
-      }}
-    >
-      {item.char}
-    </motion.div>
-  );
-};
-
 // --- Assembling Item (Atualizado) ---
 export const AssemblingItem = ({
   children,
