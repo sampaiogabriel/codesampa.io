2b5c595c1b91d3a48bb2e0901b146ad3967f3ff6
sampaiogabriel <gabrielsampaiolima@hotmail.com>
2025-12-03 17:18:33 -0300

    feat: new manifest with locale

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -30,11 +30,11 @@ export const metadata: Metadata = {
   description: 'Personal Site & Blog',
   icons: {
     icon: [
-      { url: '/favicon.ico' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
+      { url: '/favicons/favicon.ico' },
+      { url: '/favicons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicons/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
     ],
-    apple: [{ url: '/apple-touch-icon.png' }]
+    apple: [{ url: '/favicons/apple-touch-icon.png' }]
   }
 };
 
