02adb780108f3f3a6d5bffd3be0d6c08055b556f
sampaiogabriel <gabrielsampaiolima@hotmail.com>
2025-12-26 15:44:22 -0300

    feat: change redirect contact in notification

--- src/components/pages/home/shape-shifter/priority-notification.tsx
+++ src/components/pages/home/shape-shifter/priority-notification.tsx
@@ -104,7 +104,7 @@ export function PriorityNotification({
                 className="h-9 w-full bg-white text-black hover:bg-slate-200 text-xs font-bold gap-1.5"
                 asChild
               >
-                <Link href="https://cal.com/codesampa.io">
+                <Link href="/contact">
                   <Calendar size={12} />
                   {t('actions.cta')}
                 </Link>
